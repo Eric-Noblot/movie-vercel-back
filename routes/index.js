8919cbfd22ef0c72b5a61b0a978b585ee62ca669
Eric-Noblot <eric.noblot@gmail.com>
2024-02-07 14:27:38 +0100

    test vercel

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+
+var express = require('express');
+var router = express.Router();
+
+const API_KEY = process.env.API_CONNECT
+
+router.get('/movies', (req, res) => {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+    res.json({movies: data.results})
+});
+});
+
+
+module.exports = router;
+
